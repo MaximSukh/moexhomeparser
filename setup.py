4a308f505ce05c06d5d0131768290717b596207c
MaximSukh <clean89680301463@gmail.com>
2025-03-10 11:32:13 +0300

    setup.py modified

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
 setup(
     name='MOEXHOMEPARSER',
     version='0.1.0',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     description='A module for parsing MOEX financial data',
     author='Sukhanov_Maxim',
     author_email='mssukhanov@gmail.com',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
+    keywords='moex api parser ',
     install_requires=[
         'pandas',
         'numpy',
